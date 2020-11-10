a6d36cc12acb9787f1752ac156569768b9f953ea
mergwyn <gary@theclarkhome.com>
2020-11-10 21:30:33 +0000

    Asdd debugging

--- iptv/playlist.d/nord.sh
+++ iptv/playlist.d/nord.sh
@@ -4,13 +4,16 @@ set -o nounset
 set -o errexit
 set -o pipefail
 
+# shellcheck disable=1090
+source "$(dirname "${0}")"/../../functions/log.sh
+
 XMLTV=/srv/media/xmltv
 curl=/opt/puppetlabs/puppet/bin/curl
 
 DATA="$(dirname "${0}")"/../iptv_urls
 if [[ -f "${DATA}" ]] ; then
   # shellcheck disable=1090
-  . "${DATA}" 
+  source "${DATA}" 
 else
   echo "URL file ${DATA} not found" >&2
   exit 1
@@ -43,12 +46,11 @@ while IFS='~' read -r pattern1 pattern2 ; do
 done <<EOF
 EOF
 
-getChannelNumber()
-{
+getChannelNumber() {
   local pattern
   local num
   # shellcheck disable=2001
-  pattern=$(echo "$*" | sed 's/\*/\\*/')
+  pattern=$(echo "$*" | sed 's/\*/\\*/g')
   num=$(grep -ne "^$pattern\$" "${CHANNELS}" | grep -Eo '^[^:]+')
   [[ $num -eq "" ]] && num=0
   echo $num
@@ -61,32 +63,42 @@ addchannels()
   OUTPUT=${XMLTV}/${SERVICE}.m3u
   CHANNELS=${XMLTV}/${SERVICE}.channels
 
+  log_info "Adding $SERVICE channels from $URL"
+  log_debug "OUTPUT=$OUTPUT"
+  log_debug "CHANNELS=$CHANNELS"
+
   [[ -f "${CHANNELS}" ]] || touch "${CHANNELS}"
 
-  exec > "${OUTPUT}"
-  echo "#EXTM3U"
+  echo "#EXTM3U" > "${OUTPUT}"
   $curl -s "${URL}" \
     | sed -n -e "${sedscript_filter}" \
     | sed -e "${sedscript_rename}" \
     | while read -r line ; do
-        #channelID=$(echo  $line | sed 's/^.*tvg-name="\([^"]*\)\" .*$/\1/')
         # shellcheck disable=2001
         channelID=$(echo  "${line}" | sed  's/.*,\(.*\)$/\1/')
+        log_debug "Got channelID ${channelID}"
         channel=$(getChannelNumber "${channelID}")
+        log_debug "Got channel ${channel}"
         if [[ $channel = 0 ]] ; then
+          log_notice "new channel found: ${channelID}"
           echo "${channelID}" >> "${CHANNELS}"
           channel=$(getChannelNumber "${channelID}")
         fi
 
         # shellcheck disable=2001
-        echo "${line}" \
-          | sed -e "s/tvg-id/tvh-chnum=\"$channel\" &/" #-e 's/\(group-title=".*\)",/\1|H264-AAC",/'
-        read -r line
-        echo "${line}"
-      done 
-}
+        printf '%s\n' "$line" | sed -e "s/tvg-id/tvh-chnum=\"$channel\" &/" >> "${OUTPUT}"
 
-#echo ${sedscript}
+        # Get the next line contaiing the URL
+        read -r line
+        echo "${line}" >> "${OUTPUT}"
+      done 
+
+  outputLines=$(wc -l "${OUTPUT}" | cut -d ' ' -f 1)
+  if [[ ${outputLines} -le 1 ]] ; then
+    log_error "Only found ${outputLines} processing $SERVICE"
+    exit 1
+  fi
+}
 
 addchannels nord "${URL}"
 
